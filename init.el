cbe1519167fe67a90b3d36f78c356aba87a91351
Behram Mistree <behrammistree@forwardnetworks.com>
2019-05-17 23:57:50 -0700

    Remove background color settings

--- init.el
+++ init.el
@@ -156,7 +156,8 @@ There are two things you can do about this warning:
         (mouse-color      . "White")
         (cursor-color     . "White")
         (foreground-color . "gray95")
-        (background-color . "#212121")))
+))
+        ;; (background-color . "#212121")))
 
 ; Turn on highlighting with CTRL-X w h and CTRL-X w r
 (if (functionp 'global-hi-lock-mode)
@@ -451,13 +452,6 @@ There are two things you can do about this warning:
  '(load-home-init-file t t)
  '(package-selected-packages (quote (json-mode)))
  '(show-paren-mode t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#212121" :foreground "gray95" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 99 :width normal :foundry "bitstream" :family "Bitstream Vera Sans Mono")))))
 
 
 ;;bftm added named window
